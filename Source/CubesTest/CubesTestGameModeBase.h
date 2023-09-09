dc81a55eeb329634d6464044f093be8e39204880
Broosman01 <osantasov@gmail.com>
2023-09-09 16:15:43 +0300

    Added

---
+++ Source/CubesTest/CubesTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CubesTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUBESTEST_API ACubesTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
